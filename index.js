249c9f1c858ac3fdb1bc6740114cf8dcf55ff437
MelissaStiven <stivenhermite@gmail.com>
2023-04-01 17:29:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(rideDistance) {
+  if (rideDistance <= 400) {
+  return "This one is on me!";
+}else if (rideDistance > 400 && rideDistance <= 2000) {
+  return'That will be twenty bucks.'
+} else if (rideDistance > 2000 && rideDistance < 2500 ) {
+ return('I will gladly take your thirty bucks.');
+}else if (rideDistance >= 2500) {
+  return'No can do.'
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+let response = city === `NYC` ? `Ok, sounds good.` : `No go.`
+return response 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+  case "generous":
+    return("Thank you so much.");
+    case "not as generous":
+      return("Thank you.");
+      case "thanks for everything":
+      return ("Bye.")
+  }
 }
